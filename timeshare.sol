b9b0ca221cde20ead68189f8c7b406936612e0d4
Hormigasoft <hormigasoft@gmail.com>
2018-04-25 16:05:00 +0800

    Update timeshare.sol

--- timeshare.sol
+++ timeshare.sol
@@ -3,30 +3,48 @@ pragma solidity ^0.4.20;
 import "./ownable.sol";
 import "./safemath.sol";
 import "./tokens.sol";
+import "./roomtoken.sol";
 
-contract Timeshare is Ownable, ERC20 {
+contract Timeshare is Ownable, RoomToken, ERC20 {
     
     using SafeMath for uint256;
     
+    struct RoomToken {
+        string date;
+        uint64 roomNumber;
+        bool locked;
+    }
+    
+    RoomToken[] public rooms;
+
+    // ERC20
     string public symbol;
     string public  name;
     uint8 public decimals;
     uint public _totalSupply;
-
     mapping(address => uint) balances;
     mapping(address => mapping(address => uint)) allowed;
-
     // ------------------------------------------------------------------------
     // Constructor
     // ------------------------------------------------------------------------
-    function Timeshare() public {
+    function Timeshare() internal {
         //
         symbol = "UTTOK";
         name = "Utility tokens";
         decimals = 18;
         _totalSupply = 180e6;
-        balances[0x5A86f0cafD4ef3ba4f0344C138afcC84bd1ED222] = _totalSupply;
-        Transfer(address(0), 0x5A86f0cafD4ef3ba4f0344C138afcC84bd1ED222, _totalSupply);
+        balances[owner] = _totalSupply;
+        Transfer(address(0), owner, _totalSupply);
     }
+
+    function buyRoomToken(string _date, uint64 _roomNumber) public {
+        require(balances[msg.sender] == price);
+        //
+        uint id = rooms.push(RoomToken(_date, _roomNumber, true));
+        roomsToOwner[id] = msg.sender;
+        balances[msg.sender] -= price;
+        ownerRoomsCount[msg.sender]++;
+        BuyRoom(id, _date, _roomNumber);
+    }    
     
 }
